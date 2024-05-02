39473815822a024c20ef1a3603931f8518f283d7
M BHAVYA <bhavyabhavi1720@gmail.com>
2024-05-02 19:01:48 +0530

    TDL

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Fake Currency Detection
+Open Command Prompt/Terminal or IDE:
+Open the command prompt (Windows) or terminal (macOS/Linux) on your computer. You can usually find it in your system's applications menu or by searching for "Command Prompt" (Windows) or "Terminal" (macOS/Linux).
+
+IDE: Open your preferred integrated development environment (IDE), such as Visual Studio Code, PyCharm, or Jupyter Notebook.
+
+Navigate to the Directory Containing app.py:
+Use the cd command (change directory) to navigate to the directory where your app.py file is located.\
+cd path/to/directory \
+Replace path/to/directory with the actual path to the directory containing your app.py file.
+
+Activate Virtual Environment (Optional):
+If you're using a virtual environment for your project, activate it using the appropriate command.\
+source venv/bin/activate  # macOS/Linux\
+venv\Scripts\activate      # Windows\
+Replace venv with the name of your virtual environment.
+
+Install TensorFlow (if not already installed):\
+If TensorFlow is not already installed in your Python environment, you can install it using pip:\
+pip install tensorflow\
+Make sure to run this command in the same terminal or command prompt window where you're planning to run your script.\
+Run app.py:\
+Once you're in the directory containing app.py and TensorFlow is installed, you can run the script using the python command followed by the name of the script file. For example:
+python app.py
---
+++ preprocessing.py
@@ -0,0 +1,63 @@
+import cv2  # working with, mainly resizing, images
+import numpy as np  # dealing with arrays
+import os  # dealing with directories
+from random import shuffle  # mixing up or currently ordered data that might lead our network astray in training.
+from tqdm import tqdm  # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+
+IMG_SIZE = 224
+
+def label_img(img):
+    word_label = img.split('_')[-2]
+    if word_label == "500real" or word_label == '2000real':
+        return [1, 0]
+    elif word_label == '500fake' or word_label == '2000fake':
+        return [0, 1]
+
+
+def create_train_data():
+    training_data = []
+    for TRAIN_DIR in ['D:\\08\\Final Year Project Implementation\\Raw Dataset\\500_Real\\', 'D:\\08\\Final Year Project Implementation\\Raw Dataset\\500_Fake\\']:
+        for img in tqdm(os.listdir(TRAIN_DIR)):
+            label = label_img(img)
+            path = os.path.join(TRAIN_DIR, img)
+            
+            gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+            high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            lowThresh = 0.5*high_thresh
+            blur = cv2.medianBlur(gray, 17)
+            
+            edge_img = cv2.Canny(blur,lowThresh*1.5,high_thresh*1.5)
+            pts = np.argwhere(edge_img>0)
+            try:
+                y1,x1 = pts.min(axis=0)
+                y2,x2 = pts.max(axis=0)
+            except ValueError:
+                print (img)
+                pass
+            
+            cropped = gray[y1:y2, x1:x2]
+            blur_cropped = cv2.medianBlur(cropped, 7)
+            blur_cropped = cv2.resize(blur_cropped , (IMG_SIZE, IMG_SIZE))
+            training_data.append([np.array(blur_cropped), np.array(label)])
+            
+            #Applying Salt and Pepper filter
+            s_vs_p = 0.5
+            amount = 0.02
+            for j in range(0,5):
+                sp_img = np.copy(blur_cropped)
+                # Salt mode
+                num_salt = np.ceil(amount * blur_cropped.size * s_vs_p)
+                coords = [np.random.randint(0, i - 1, int(num_salt)) for i in blur_cropped.shape]
+                sp_img[coords] = 255
+                # Pepper mode
+                num_pepper = np.ceil(amount* blur_cropped.size * (1. - s_vs_p))
+                coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in blur_cropped.shape]
+                sp_img[coords] = 0
+                training_data.append([np.array(sp_img), np.array(label)])
+            
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+train_data = create_train_data()
+print(len(train_data))
